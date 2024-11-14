1cfa0b1a981847283edda28e3e1c8883a3413dca
Rodrigo A. Temoteo <rodreturn@gmail.com>
2024-11-14 02:51:01 -0300

    Primeirocommit do projeto

---
+++ src/main/java/com/ratecnologias/minha_jornada/MinhaJornadaApiApplication.java
@@ -0,0 +1,15 @@
+package com.ratecnologias.minha_jornada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaJornadaApiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MinhaJornadaApiApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/ratecnologias/minha_jornada/MinhaJornadaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratecnologias.minha_jornada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhaJornadaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
